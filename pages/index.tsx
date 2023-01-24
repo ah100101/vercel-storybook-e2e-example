2e5303e79ce7243bff91d01231c396a69f3b63a5
Alex Hawley <alex.hawley@vercel.com>
2023-01-24 14:54:45 -0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            V5 - Get started by editing&nbsp;
+            V6 - Get started by editing&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
           </p>
           <div>
